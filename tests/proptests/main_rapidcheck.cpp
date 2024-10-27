4a855dbcb03f0077ab9f0f9bf5996c0afb8ef7fd
Matthias Möller <m_moeller@live.de>
2024-10-27 10:13:11 +0100

    testing childs of depth 1

--- tests/proptests/main_rapidcheck.cpp
+++ tests/proptests/main_rapidcheck.cpp
@@ -110,10 +110,32 @@ int main()
                   RC_ASSERT(in == to_test);
               });
 
-    rc::check("single line comment should not cause any errors",
-              [](const std::string &comment)
+    rc::check("check if the childs are also written and parsed correctly",
+              [](vdf::object in)
               {
-                  RC_PRE(comment.find("\n") == comment.npos);
+                  // todo this just tests childs with depth 1
+                  using child_vec = std::vector<std::shared_ptr<vdf::object>>;
+                  child_vec childs = *rc::gen::container<child_vec>(
+                      rc::gen::makeShared<vdf::object>(
+                          rc::gen::arbitrary<vdf::object>()));
+
+                  for (const auto &c : childs)
+                  {
+                      in.childs[c->name] = c;
+                  }
+
+                  std::stringstream sstr;
+                  vdf::write(sstr, in);
+                  auto to_test = tyti::vdf::read(sstr);
+                  RC_ASSERT(in == to_test);
+              });
+
+    rc::check("single line comment should not cause any errors",
+              []()
+              {
+                  auto comment = *rc::gen::suchThat(
+                      rc::gen::string<std::string>(), [](const std::string &str)
+                      { return str.find("\n") == str.npos; });
 
                   std::stringstream input;
                   input << "\"test\""
@@ -128,9 +150,11 @@ int main()
               });
 
     rc::check("multi line comment should not cause any errors",
-              [](const std::string &comment)
+              []()
               {
-                  RC_PRE(comment.find("*/") == comment.npos);
+                  auto comment = *rc::gen::suchThat(
+                      rc::gen::string<std::string>(), [](const std::string &str)
+                      { return str.find("*/") == str.npos; });
 
                   std::stringstream input;
                   input << "\"test\""
