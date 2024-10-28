19283a879dfe949471c91bc998f4e23799672602
Matthias Möller <m_moeller@live.de>
2024-10-28 17:54:17 +0100

    add strip option to fuzzing

--- fuzzing/main.cpp
+++ fuzzing/main.cpp
@@ -1,13 +1,19 @@
 #include <cstdint>
 #include <string_view>
 
-#include <vdf_parser.hpp>
 #include <iostream>
+#include <vdf_parser.hpp>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    std::string_view test_corpus{reinterpret_cast<const char *>(data), size};
+    if (size == 0)
+        return 0;
+    std::string_view test_corpus{reinterpret_cast<const char *>(data + 1),
+                                 size};
+    tyti::vdf::Options opts;
+    opts.strip_escape_symbols = data[0] != 0;
     bool ok;
-    auto result = tyti::vdf::read(test_corpus.begin(), test_corpus.end(), &ok);
+    auto result =
+        tyti::vdf::read(test_corpus.begin(), test_corpus.end(), &ok, opts);
     return 0;
 }
